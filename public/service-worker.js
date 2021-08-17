7f44cdb72126788d34457f7383103e5c16379811
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-17 19:28:31 -0400

    Added fetch callback function to event listener in service worker to retrieve cached resources when they exist and fallback to a network request when they don't exist.

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,21 +8,42 @@ const files_to_cache = [
 const precache = 'precache';
 const runtime = 'runtime';
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches
-      .open(precache)
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(precache)
+    //caches must all be retrieved on install fails
       .then((cache) => cache.addAll(files_to_cache))
       .then(self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", e => {
   const cachesUsed = [precache, runtime];
-  event.waitUntil(
+  e.waitUntil(
     caches.keys()
+      //array.filter returns those caches not matching cachesUsed
       .then(cacheNames => cacheNames.filter(cacheName => !cachesUsed.includes(cacheName)))
+      //and then deletes them
       .then(cachesToDelete => Promise.all(cachesToDelete.map(cacheToDelete => caches.delete(cacheToDelete))))
       .then(() => self.clients.claim())
   );
 });
+
+self.addEventListener('fetch', (e) => {
+  if (e.req.url.startsWith(self.location.origin)) {
+    e.respondWith(caches.match(e.req)
+    .then((cacheRes) => {
+        //Use cache if it exists
+        if (cacheRes != undefined)
+          return cacheRes;
+        //otherwise fallback to retrieving this resource from the network
+        else {
+          return caches.open(runtime).then((cache) => fetch(e.req)
+            .then((res) => cache.put(e.req, res.clone())
+            .then(() => res)));
+        }
+      })
+    );
+  }
+});
+
