93738dab1d666d1d1334de2f33b96576428e50a3
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-18 01:02:45 -0400

    changed name of mongo database

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
 });
 
 // routes
