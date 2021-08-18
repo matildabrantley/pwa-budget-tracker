768dcaf73829877d96aa35543a6e662aadee87ec
Matilda Brantley <matildahbrantley@gmail.com>
2021-08-17 20:29:10 -0400

    Caching API responses handled in fetch callback now, as well as serving static assets for non-API requests.

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,18 +9,16 @@ const precache = 'precache';
 const runtime = 'runtime';
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(precache)
+  e.waitUntil( caches.open(precache)
     //caches must all be retrieved on install fails
       .then((cache) => cache.addAll(files_to_cache))
       .then(self.skipWaiting())
   );
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", (e) => {
   const cachesUsed = [precache, runtime];
-  e.waitUntil(
-    caches.keys()
+  e.waitUntil( caches.keys()
       //array.filter returns those caches not matching cachesUsed
       .then(cacheNames => cacheNames.filter(cacheName => !cachesUsed.includes(cacheName)))
       //and then deletes them
@@ -30,20 +28,39 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.req.url.startsWith(self.location.origin)) {
-    e.respondWith(caches.match(e.req)
+  if (e.request.url.startsWith(self.location.origin)) {
+    e.respondWith( caches.match(e.request)
     .then((cacheRes) => {
         //Use cache if it exists
         if (cacheRes != undefined)
           return cacheRes;
         //otherwise fallback to retrieving this resource from the network
         else {
-          return caches.open(runtime).then((cache) => fetch(e.req)
-            .then((res) => cache.put(e.req, res.clone())
+          return caches.open(runtime).then((cache) => fetch(e.request)
+            .then((res) => cache.put(e.request, res.clone())
             .then(() => res)));
         }
       })
     );
   }
+  //cache API response
+  else if (e.request.url.includes("/api/")) {
+    e.respondWith( caches.open(runtime)
+      .then(cache => {
+         return fetch(e.request)
+          .then(res => {
+            cache.put(e.request, res.clone());
+            return res;
+          })
+          .catch(() => caches.match(e.request));
+      })
+    );
+    return;
+  }
+
+  //serve static assets for non-API requests
+  e.respondWith( caches.open(precache)
+    .then(cache => cache.match(e.request).then(res => res || fetch(e.request)))
+  );
 });
 
